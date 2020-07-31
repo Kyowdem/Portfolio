90546a3beb5f57787e34aa4f6f738b362167d6c7
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-07-31 10:41:40 -0400

    add navbar to App.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
-
-export default {
-  components: {
-    Navbar
-  }
-}
+export default {};
 </script>
 
 <style>
-
 </style>
