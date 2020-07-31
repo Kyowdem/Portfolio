b4c5ae0fa9caee76f65212b595baf7a0dc343c4f
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-07-31 10:06:46 -0400

    rename to navbar.vue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.component('navbar', () => import('./components/Navbar.js'))
-
 Vue.config.productionTip = false
 
 new Vue({
