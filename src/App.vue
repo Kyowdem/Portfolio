f08e81cf91150d543f1cbd050301a5e241903b88
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-07-31 11:02:43 -0400

    beautify app

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   components: {
-    Navbar
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style>
