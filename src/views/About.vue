e9d222b6c37b433de14b9bde7b00dbb3ca8a1cd9
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-07-31 10:14:34 -0400

    add test in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Navbar></Navbar>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
