b0e88ef0ba1fab38be65e7926a62ad7e58c401ea
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-07-31 09:59:16 -0400

    add navbar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+Vue.component('navbar', () => import('./components/Navbar.js'))
+
 Vue.config.productionTip = false
 
 new Vue({
