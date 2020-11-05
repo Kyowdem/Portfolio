8bfb9657ca3f095a90b1b7deb97f5842e973c58d
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-11-04 20:51:11 -0500

    add realisation components

---
+++ src/components/Realisation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div data-aos="fade-right" class="row">
+    <div class="col-lg-12 col-md-12">
+      <p>On m’a donné ce modèle UML.</p>
+    </div>
+    <div class="col-lg-12 col-md-12">
+      <img
+        :src="
+          require('@/' +
+            this.imgSchPath +
+            'PPE/2eme_annee/GSB/controler_visiteur_read.png')
+        "
+        alt="controler_visiteur_read.png"
+        class="img-fluid centerImg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        p: "",
+        imgPath: ""
+    }
+};
+</script>
+
+<style>
+</style>
