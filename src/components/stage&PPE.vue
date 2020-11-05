405ffc15bfa5dbe9917e81fa00e7d23d82418129
Collomb Mehdi <collomb.mehdi@gmail.com>
2020-11-05 10:41:05 -0500

    transform to realisation: jQuery

--- src/components/stage&PPE.vue
+++ src/components/stage&PPE.vue
@@ -64,99 +64,15 @@
             </div>
 
             <div v-if="name == 'jQuery.ajax, le chargement des pages'">
-              <div data-aos="fade-right" class="row">
-                <div class="col-lg-12 col-md-12">
-                  <p>
-                    J’ai beaucoup utilisé la documentation de jQuery et d’Ajax,
-                    qui sont très bien faites. La présence de mon tuteur m’a
-                    aussi permis de mieux m’initier là-dedans.
-                  </p>
-                </div>
-              </div>
-              <div data-aos="fade-right" class="row">
-                <div class="col-lg-12 col-md-12">
-                  <p>
-                    Après plusieurs modifications, cette page était utilisée
-                    comme principale pour le site. Cette dernière complètement
-                    orienté objet permet la facilité au futur modification
-                    qu’ils voudront apporter.
-                  </p>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                  <img
-                    :src="
-                      require('@/' +
-                        this.imgSchPath +
-                        'stage/1ere_annee/accueil(menuOuvert).png')
-                    "
-                    alt="Accueil(menuOuvert).png"
-                    class="img-fluid"
-                  />
-                </div>
-              </div>
-              <div data-aos="fade-right" class="row">
-                <div class="col-lg-12 col-md-12">
-                  <p>
-                    Dans ce menu, quand on allait dans un contexte, par exemple
-                    une entrée, on se retrouvait sur cette page qui étais généré
-                    complètement en JavaScript et faisais charger les données
-                    des menus déroulant grâce à Ajax, car nous avions besoin de
-                    charger les données sans recharger la page.
-                  </p>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                  <img
-                    :src="
-                      require('@/' +
-                        this.imgSchPath +
-                        'stage/1ere_annee/entre.png')
-                    "
-                    alt="Entre.png"
-                    class="img-fluid"
-                  />
-                </div>
-              </div>
-              <div data-aos="fade-right" class="row">
-                <div class="col-lg-12 col-md-12">
-                  <p>
-                    Je pouvais ensuite me rediriger vers les autres pages lors
-                    d’un clic sur le bouton suivant ou précédent, sans changer
-                    de page c'était juste un remplacement de div, et donc
-                    lorsque je cliquais cela faisais charger grâce à Ajax les
-                    données de la page concerné.
-                  </p>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                  <img
-                    :src="
-                      require('@/' +
-                        this.imgSchPath +
-                        'stage/1ere_annee/equipement.png')
-                    "
-                    alt="equipement.png"
-                    class="img-fluid"
-                  />
-                </div>
-              </div>
-              <div data-aos="fade-right" class="row">
-                <div class="col-lg-12 col-md-12">
-                  <p>
-                    On peut voir sur un bout de code ci-dessous où lors d’un
-                    clic, Ajax allais faire charger la page souhaité.
-                  </p>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                  <img
-                    :src="
-                      require('@/' +
-                        this.imgSchPath +
-                        'stage/1ere_annee/ajax_code.png')
-                    "
-                    alt="ajax_code.png"
-                    class="img-fluid"
-                  />
-                </div>
-              </div>
+              <Realisation p="J’ai beaucoup utilisé la documentation de jQuery et d’Ajax, qui sont très bien faites. La présence de mon tuteur m’a aussi permis de mieux m’initier là-dedans."></Realisation>
+
+              <Realisation p="Après plusieurs modifications, cette page était utilisée comme principale pour le site. Cette dernière complètement orienté objet permet la facilité au futur modification qu’ils voudront apporter." imgPath="stage/1ere_annee/accueil(menuOuvert).png"></Realisation>
+
+              <Realisation p="Dans ce menu, quand on allait dans un contexte, par exemple une entrée, on se retrouvait sur cette page qui étais généré complètement en JavaScript et faisais charger les données des menus déroulant grâce à Ajax, car nous avions besoin de charger les données sans recharger la page." imgPath="stage/1ere_annee/entre.png"></Realisation>
+
+              <Realisation p="Je pouvais ensuite me rediriger vers les autres pages lors d’un clic sur le bouton suivant ou précédent, sans changer de page c'était juste un remplacement de div, et donc lorsque je cliquais cela faisais charger grâce à Ajax les données de la page concerné." imgPath="stage/1ere_annee/equipement.png"></Realisation>
+
+              <Realisation p="On peut voir sur un bout de code ci-dessous où lors d’un clic, Ajax allais faire charger la page souhaité." imgPath="stage/1ere_annee/ajax_code.png"></Realisation>
             </div>
 
             <div v-if="name == 'Partie visuel, signalement d’une erreur. jQuery-ui'">
