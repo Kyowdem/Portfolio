5b181e32ff70638c8ba713bbe47b01e56bf2820f
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-04 22:01:30 -0500

    delete all animation

--- src/views/Etude.vue
+++ src/views/Etude.vue
@@ -7,81 +7,11 @@
         v-if="$route.params.etudeName == 'BTS'"
         class="row justify-content-md-around activitiesList"
       >
-        <div
-          v-show="show.PPE"
-          class="col-md-6 col-lg-4"
-          @mouseover="
-            animateTwoItems(
-              [
-                { query: 'img[name=PPE]', animation: 'flipOutY' },
-                { query: 'table[name=PPE]', animation: 'flipInY' },
-              ],
-              { name: 'PPE', bool: false }
-            )
-          "
-        >
-          <img
-            name="PPE"
-            :src="require('@/assets/img/school/PPE.jpg')"
-            class="img-fluid"
-            alt="PPE"
-          />
-          <div id="mouseoverPPE" role="tooltip">
-            Passez la souris sur la photo :D
-          </div>
-        </div>
-        <div
-          class="col-md-6 col-lg-4"
-          v-show="!show.PPE"
-          @mouseleave="
-            animateTwoItems(
-              [
-                { query: 'table[name=PPE]', animation: 'flipOutY' },
-                { query: 'img[name=PPE]', animation: 'flipInY' },
-              ],
-              { name: 'PPE', bool: true }
-            )
-          "
-        >
+        <div class="col-md-6 col-lg-4">
           <Table name="PPE"></Table>
         </div>
 
-        <div
-          v-show="show.stage"
-          class="col-md-6 col-lg-4"
-          @mouseover="
-            animateTwoItems(
-              [
-                { query: 'img[name=stage]', animation: 'flipOutY' },
-                { query: 'table[name=stage]', animation: 'flipInY' },
-              ],
-              { name: 'stage', bool: false }
-            )
-          "
-        >
-          <img
-            name="stage"
-            :src="require('@/assets/img/school/stage.jpg')"
-            class="img-fluid"
-            alt="stage"
-          />
-          <div id="mouseoverStage" role="tooltip">
-            Passez la souris sur la photo :D
-          </div>
-        </div>
-        <div
-          class="col-md-6 col-lg-4"
-          v-show="!show.stage"
-          @mouseleave="
-            animateTwoItems(
-              [
-                { query: 'table[name=stage]', animation: 'flipOutY' },
-                { query: 'img[name=stage]', animation: 'flipInY' },
-              ],
-              { name: 'stage', bool: true }
-            )
-          "
-        >
+        <div class="col-md-6 col-lg-4">
           <Table name="stage"></Table>
         </div>
       </div>
@@ -101,8 +31,6 @@
 </template>
 
 <script>
-import $ from "jquery";
-import { createPopper } from "@popperjs/core";
 // Components
 import School from "@/components/School.vue";
 import StagePPE from "@/components/stage&PPE.vue";
@@ -125,13 +53,6 @@ export default {
   mounted() {
     this.checkActivityURL();
     this.setActivityOrSchool();
-
-    if (this.$route.params.etudeName == "BTS") {
-      this.addAnimations("img[name=PPE]", "shakeY");
-      this.addAnimations("img[name=stage]", "shakeY");
-      this.createPopper("img[name=PPE]", "#mouseoverPPE");
-      this.createPopper("img[name=stage]", "#mouseoverStage");
-    }
   },
   watch: {
     "$route.params.activity": function () {
@@ -150,50 +71,6 @@ export default {
     checkActivityURL() {
       if (this.$route.params.activity)
         this.nameOfActivityClick = this.$route.params.activity;
-    },
-    // query: String, animation: Array
-    addAnimations(query, animations) {
-      $(query).addClass(
-        "animate__animated" + " " + this.getAnimation(animations)
-      );
-
-      this.deleteAnimations(query, animations);
-    },
-    // query: String, animation: Array
-    deleteAnimations(query, animations) {
-      $(query).bind("animationend", () => {
-        $(query).removeClass(
-          "animate__animated" + " " + this.getAnimation(animations)
-        );
-        $(query).unbind("animationend");
-      });
-    },
-    // animation: String or Array of string
-    getAnimation(animations) {
-      if (typeof animations == "string") animations = [animations];
-
-      let animation = "";
-      animations.forEach((element) => {
-        animation += `animate__${element} `;
-      });
-      return animation.trim();
-    },
-    // elementShow: {name: "", bool: true}
-    // items: [{query: queryElement, animation: Animate.css}, {...}].length: 2.
-    animateTwoItems(items, elementShow) {
-      this.addAnimations(items[0].query, items[0].animation);
-
-      $(items[0].query).bind("animationend", () => {
-        this.show[elementShow.name] = elementShow.bool;
-        this.addAnimations(items[1].query, items[1].animation);
-        $(items[0].query).unbind("animationend");
-      });
-    },
-    createPopper(queryElement, queryText, placement = "top", time = 2000) {
-      createPopper($(queryElement)[0], $(queryText)[0], { placement });
-      setTimeout(() => {
-        $(queryText).fadeOut();
-      }, time);
     },
   },
 };
