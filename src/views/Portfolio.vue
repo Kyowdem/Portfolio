2cbeb8249521217804550d1d914b5d151b6223b4
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-14 23:45:57 -0400

    add filter

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="container">
-    <div class="row filter">foo</div>
+    <div class="row filter">
+      <div
+        class="form-check form-check-inline"
+        v-for="filter in filters"
+        :key="filter"
+      >
+        <input
+          class="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          v-model="selectedFilter"
+          :id="filter"
+          :value="filter"
+        />
+        <label class="form-check-label" :for="filter">{{ filter }}</label>
+      </div>
+    </div>
     <div class="row project">bar</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Portfolio",
+  data() {
+    return {
+      filters: ["vue.js", "javascript", "HTML", "CSS", "SQL"],
+      selectedFilter: "",
+    };
+  },
+};
 </script>
 
 <style>
