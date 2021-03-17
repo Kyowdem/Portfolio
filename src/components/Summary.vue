cb3886c14051172804059dc2cb9e5e2d8efb1648
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-17 15:52:14 -0400

    create Summary components

---
+++ src/components/Summary.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h3 class="titre">{{ title }}</h3>
+    <p class="text">{{ describe }}</p>
+    <div class="row">
+      <div class="col-md-2 filter"><p>filter:</p></div>
+      <div class="col-md-8">
+        <p v-for="filter in filters" :key="filter">{{ filter }}</p>
+      </div>
+      <div class="col-md-2 date">
+        <p>{{ date }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    describe: String,
+    filters: Array,
+    date: String,
+  },
+};
+</script>
+
+<style>
+.project > div {
+  background: rgba(128, 128, 128, 0.4);
+  box-shadow: 5px 5px 5px grey;
+}
+.project .date {
+  text-align: end;
+}
+.project .text {
+  text-align: justify;
+}
+.project .titre,
+.project .filter {
+  text-align: start;
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -19,26 +19,20 @@
     </div>
     <div class="row project">
       <div class="col-md-12">
-        <h3 class="titre">Titre</h3>
-        <p class="text">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
-          culpa voluptate eligendi tempore fuga, recusandae facilis voluptates
-          modi eveniet veniam doloribus officia maiores dolor facere quae earum
-          numquam optio sunt.
-        </p>
-        <div class="row">
-          <div class="col-md-2 filter"><p>filter:</p></div>
-          <div class="col-md-8"></div>
-          <div class="col-md-2 date"><p>AAAA/MM/JJ</p></div>
-        </div>
+        <Summary title="Titre" describe="lorem ..." :filters="['C++']" date="AAAA/MM/JJ"></Summary>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Summary from '@/components/Summary.vue'
+
 export default {
   name: "Portfolio",
+  components: {
+    Summary
+  },
   data() {
     return {
       filters: ["vue.js", "javascript", "HTML", "CSS", "SQL", "C#", "C++"],
@@ -49,21 +43,6 @@ export default {
 </script>
 
 <style>
-.project > div {
-  background: rgba(128, 128, 128, 0.4);
-  box-shadow: 5px 5px 5px grey;
-}
-.project .date {
-  text-align: end;
-}
-.project .text {
-  text-align: justify;
-}
-.project .titre,
-.project .filter {
-  text-align: start;
-}
-
 .filter .form-check,
 .filter .form-check > label,
 .filter .form-check > input {
