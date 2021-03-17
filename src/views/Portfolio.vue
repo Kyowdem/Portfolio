b8e3ce205c463031c2d69905dbe7af7f669b8b21
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-17 15:36:28 -0400

    beautify and setup pointer to radio box

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -20,9 +20,14 @@
     <div class="row project">
       <div class="col-md-12">
         <h3 class="titre">Titre</h3>
-        <p class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum culpa voluptate eligendi tempore fuga, recusandae facilis voluptates modi eveniet veniam doloribus officia maiores dolor facere quae earum numquam optio sunt.</p>
+        <p class="text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+          culpa voluptate eligendi tempore fuga, recusandae facilis voluptates
+          modi eveniet veniam doloribus officia maiores dolor facere quae earum
+          numquam optio sunt.
+        </p>
         <div class="row">
-          <div class="col-md-2 filter"><p>filter: </p></div>
+          <div class="col-md-2 filter"><p>filter:</p></div>
           <div class="col-md-8"></div>
           <div class="col-md-2 date"><p>AAAA/MM/JJ</p></div>
         </div>
@@ -36,7 +41,7 @@ export default {
   name: "Portfolio",
   data() {
     return {
-      filters: ["vue.js", "javascript", "HTML", "CSS", "SQL"],
+      filters: ["vue.js", "javascript", "HTML", "CSS", "SQL", "C#", "C++"],
       selectedFilter: "",
     };
   },
@@ -44,17 +49,24 @@ export default {
 </script>
 
 <style>
-.project>div {
+.project > div {
   background: rgba(128, 128, 128, 0.4);
   box-shadow: 5px 5px 5px grey;
 }
 .project .date {
-  text-align: end
+  text-align: end;
 }
 .project .text {
   text-align: justify;
 }
-.project .titre, .project .filter {
+.project .titre,
+.project .filter {
   text-align: start;
+}
+
+.filter .form-check,
+.filter .form-check > label,
+.filter .form-check > input {
+  cursor: pointer;
 }
 </style>
