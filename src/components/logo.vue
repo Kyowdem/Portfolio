e245f1d1c14713dfa905adf2248987f82aa024c6
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-19 11:39:07 -0400

    set logo

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-      
+    <img
+      :width="width"
+      :src="require(`@/assets/img/icon/${imgName.toLowerCase()}-logo.png`)"
+      :alt="imgName"
+      :title="imgName"
+    />
   </div>
 </template>
 
 <script>
 export default {
-    
-}
+    props: {
+        imgName: String,
+        width: String
+    }
+};
 </script>
 
 <style>
-
 </style>
