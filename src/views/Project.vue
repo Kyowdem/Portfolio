d503523b6e3e473d7ebb2517ff167805b5e7f3bd
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-03-19 22:00:34 -0400

    add 4 last

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -77,10 +77,50 @@
 
       <Realisation p="Une première ligne disant que le fichier est déjà droit alors pas besoin de le retourner, il m’affiche en second le prix et le reste se sont simplement des indications qui explique que le programme a été arrêter et que le fichier texte a bien été supprimer."></Realisation>
     </div>
-    <div v-if="projectName == projects[4].name">Foo</div>
-    <div v-if="projectName == projects[5].name">Foo</div>
-    <div v-if="projectName == projects[6].name">Foo</div>
-    <div v-if="projectName == projects[7].name">Foo</div>
+    <div v-if="projectName == projects[4].name">
+      <Realisation p="Dans le PPE, les tables et les colonnes pour le jeu d’essai étaient déjà existants dans un fichier au format SQL. Avant d’insérer les tables et les données, il fallait d’abord créer la base de données." :imgPath="['PPE/1ere_annee/create_database.png']"></Realisation>
+
+      <Realisation p="Après avoir créé la base, j’ai ouvert le fichier .sql dans un éditeur de texte pour pouvoir copier coller tout le script des tables et du jeux d’essai dans MYSQL." :imgPath="['PPE/1ere_annee/scriptSQL.png','PPE/1ere_annee/inserts.png']"></Realisation>
+
+      <Realisation p="Une suite de requête était demandée par l’entreprise, elle voulait par exemple savoir le prix moyen d’un bagel, une requête plutôt simple et courte." :imgPath="['PPE/1ere_annee/req2.png', 'PPE/1ere_annee/rep_req2.png']"></Realisation>
+
+      <Realisation p="La requête du dessous est un peu plus longue car on voulait les ingrédients d’un bagel spécifique, « le chèvre miel »." :imgPath="['PPE/1ere_annee/req1.png', 'PPE/1ere_annee/rep_req1.png']"></Realisation>
+    </div>
+    <div v-if="projectName == projects[5].name">
+      <Realisation p="Dans notre groupe, je me suis occupé de la partie design et dynamique du site. Pour cela, j’ai utilisé jQuery et Ajax. J’ai également utilisé la librairie slick, afin de produire un carrousel qui répondais à mes attentes et qui était très facile d’utilisation." :imgPath="['PPE/2eme_annee/Judge_movies/accueil.png']"></Realisation>
+
+      <Realisation p="Mais pour voir un film et l’acheter il faut d’abord se connecter. Pour donner un rendu tout aussi simple que dynamique et en mettant surtout une petite touche de jaune aux pages login et inscription, j’ai paramétré un fond avec des particules animées dans un fond noir grâce à l’outil « particules.js »." :imgPath="['PPE/2eme_annee/Judge_movies/login.png']"></Realisation>
+
+      <Realisation p="Une fois le login crée ainsi que la page d’accueil avec le carrousel des films, lors du clic il fallait afficher une page simple et semblable pour toute. Je me suis donc occupé de la page film en faisant une page qui envoyais directement le lien de la bande annonce du film, son titre, ces réalisateurs, ces acteurs et ça note sur le site." :imgPath="['PPE/2eme_annee/Judge_movies/film_haut.png']" pAfterImg="Sur la photo la balise iframe a juste un petit problème"></Realisation>
+
+      <Realisation p="La page faisait juste charger les données depuis la base mais j’ai quand même voulu garder l’aspect dynamique alors j’ai juste rajouté des étoiles pour que l’utilisateur note sur 5 le film." :imgPath="['PPE/2eme_annee/Judge_movies/film_bas.png', 'PPE/2eme_annee/Judge_movies/code_etoile_film.png']"></Realisation>
+
+      <Realisation p="En tant qu’admin nous pouvons rajouter des films et même cela est animé." :imgPath="['PPE/2eme_annee/Judge_movies/ajout_film.png']"></Realisation>
+
+      <Realisation p="Si un Acteur n’est pas dans la liste, nous avons juste à cliquer sur le bouton à droite pour faire apparaitre un formulaire de saisie. Ce dernier ajoute l’acteur au select sans avoir à rafraichir la page, elle se charge automatiquement grâce à Ajax." :imgPath="['PPE/2eme_annee/Judge_movies/ajoute_real.png']"></Realisation>
+    </div>
+    <div v-if="projectName == projects[6].name">
+      <Realisation p="J’ai commencé par faire la partie graphique du site avant de commencer tout le back-end. Je voulais que la page de recherche soit éducative, simple d’utilisation. J’ai alors mis une barre de recherche au milieu de l’écran et qui montera lorsqu’on saisit une information." :imgPath="['PPE/2eme_annee/GSB/barre_recherche.png', 'PPE/2eme_annee/GSB/recherche_visiteur.png']"></Realisation>
+
+      <Realisation p="La recherche d’un visiteur se fait grâce à un web service où la méthode GET est utilisé. On envoie les données GET dans l’url en tapant simplement le nom de la page suivis d’un point d’interrogation, le nom de la variable et la donnée à envoyer. On peut aussi tester notre Api avec Postman qui est un environnement de développement pour les API." :imgPath="['PPE/2eme_annee/GSB/postman_visiteur_id.png']"></Realisation>
+
+      <Realisation p="On récupère donc la variable “id“ dans le script read.php sous la forme d’une variable dans la collection $_GET. Pour me simplifier, je fais un parcours de celle-ci pour garder dans la variable $getVariable et pouvoir faire ensuite un switch case." :imgPath="['PPE/2eme_annee/GSB/controler_visiteur_read.png']"></Realisation>
+
+      <Realisation p="On voit que lorsqu’il récupère l’Id il va dans la fonction “readById“ dans la classe “tableVisiteur“. Dans cette fonction on peut voir que la requête est préparée avant d’être exécutée et retournée vers le Controller read.php." :imgPath="['PPE/2eme_annee/GSB/metier_visiteur_update.png']" pAfterImg="Pour finir, le résultat de la requête est traité pour être convertie sous le format JSON. On peut maintenant récupérer le résultat de la requête, comme nous l’avons vu plus haut avec Postman."></Realisation>
+
+      <Realisation p="Pour un ajout, une modification ou une suppression, c’est à peu près la même chose sauf qu’au lieu d’utiliser GET nous prendrons : POST pour un ajout, PUT pour une modification et DELETE pour une suppression. L’envoie de donnée ne se passe pas par l’url du web service mais par l’envoie des données en JSON et le traitement de ces données dans le Controller." :imgPath="['PPE/2eme_annee/GSB/postman_visiteur_update.png', 'PPE/2eme_annee/GSB/metier_visiteur_update.png', 'PPE/2eme_annee/GSB/modif_visiteur.png']"></Realisation>
+
+      <Realisation p="Revenons à notre recherche. Après avoir fait la recherche par nom nous avons une collection de visiteur que je stock dans une variable locale pour réutiliser les données. Je fais ensuite un v-for pour parcourir tous les visiteurs et faire afficher le tableau." :imgPath="['PPE/2eme_annee/GSB/code_rechercher_visiteur.png']"></Realisation>
+    </div>
+    <div v-if="projectName == projects[7].name">
+      <Realisation p="On m’a donné ce modèle UML." :imgPath="['ecole/INF3105/INF3105-Pile.png']"></Realisation>
+
+      <Realisation p="A partir de ça, on a dû compléter chaque fonction" :imgPath="['ecole/INF3105/class_pile.png']"></Realisation>
+
+      <Realisation p="On a ensuite étudié l’arbre AVL, ainsi que plusieurs arbres comme rouge-noir ou encore des arbres map. Pour l’instant, l’arbre rouge-noir est celui que je n’ai pas implémenter mais que j’ai quand même pu apprendre schématiquement et logiquement. Encore une fois on nous a donner un modèle UML pour l’arbre AVL." :imgPath="['ecole/INF3105/INF3105-ArbreAVL.png']"></Realisation>
+
+      <Realisation p="L’arbre AVL a été assez dur à implémenter à cause des concepts avancer du C++, comme les pointeurs, les références, des Template grâce au type T et d’autre détails du au compilateur ou directement au langage. La fonction la plus compliquer a été l’insertion, ainsi que les rotations, du a l’arbre qui se veut comme ça. Le code de l’insertion est surtout basé sur un équilibrage et donc des rotations." :imgPath="['ecole/INF3105/class_arbreAVL-insertion.png']"></Realisation>
+    </div>
   </div>
 </template>
 
